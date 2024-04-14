701efead0506bfbd757fe3a132f2a8f4538865ab
milenamrugala <milena.mrugala@icloud.com>
2024-04-14 14:36:24 +0200

    Group class

--- src/main/kotlin/Group.kt
+++ src/main/kotlin/Group.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 class Group {
     private val students = mutableMapOf<String, Student>()
 
@@ -5,21 +7,20 @@ class Group {
         if (students.containsKey(student.getId())) {
             throw IllegalArgumentException("Student ${student.getId()} already exists, cannot add a new student.")
         }
-        if (student.getGrade() !in 2.0..5.0) {
-            throw IllegalArgumentException("Invalid grade. The range of grade is 2.0-5.0")
-        }
         students[student.getId()] = student
     }
 
-    fun avg(): Double {
-        var sum = 0.0
-        for ((_, student) in students) {
-            sum += student.getGrade()
-        }
-        return sum / students.size
+    fun getStudents(): Collection<Student> {
+        return students.values
     }
 
-    fun number(): Int {
-        return students.size
+    fun statistics(): Stats {
+        val grades = students.values.map { it.getGrade().grade }
+        val min = grades.minOrNull() ?: 0.0
+        val max = grades.maxOrNull() ?: 0.0
+        val avg = grades.average()
+        val stdDev = kotlin.math.sqrt(grades.map { (it - avg).pow(2.0) }.average())
+        return Stats(min, max, avg, stdDev)
     }
+
 }
